0c96230d3a13f50ede30884c7afdc6b6c83de122
SabbirSami <smd71430@gmail.com>
2022-03-25 22:28:58 +0600

    bootstrap, icon install

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <h1 className="text-danger">Sabbir</h1>
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
